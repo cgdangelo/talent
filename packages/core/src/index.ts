68399c35f846b00f84230b14c4e9b3d9957b94af
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-06-19 16:58:54 -0400

    Add fp-ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,10 @@
+import { taskEither } from "fp-ts";
+import { constant, pipe } from "fp-ts/lib/function";
 import { readFile } from "fs/promises";
 import { resolve } from "path";
 
 const DEMO_PATH = resolve(__dirname, "../../../demo.dem");
+
 const DIRECTORY_ENTRY_LENGTH =
   4 + // Unknown
   64 + // Title
@@ -21,49 +24,56 @@ const readDirectoryEntry = (buffer: Buffer, cursor = 0) => ({
   time: buffer.readFloatLE(cursor + 4 + 64 + 4 + 4),
 });
 
-readFile(DEMO_PATH).then((buffer) => {
-  const magic = readString(buffer, 0, 8);
+pipe(
+  taskEither.tryCatch(
+    () => readFile(DEMO_PATH),
+    constant("could not read file")
+  ),
 
-  console.assert(magic !== "HLDEMO", `File is not an HLDEMO: ${magic}`);
+  taskEither.map((buffer) => {
+    const magic = readString(buffer, 0, 8);
 
-  const protocol = buffer.readInt32LE(8);
-  const networkProtocol = buffer.readInt32LE(12);
-  const mapName = readString(buffer, 13, 260);
-  const gameDirectory = readString(buffer, 274, 260);
-  const mapChecksum = buffer.readUInt32LE(535);
+    console.assert(magic === "HLDEMO", `File is not an HLDEMO: ${magic}`);
 
-  // Validate directory entries offset
-  const directoryEntriesOffset = buffer.readUInt32LE(540);
-  const expectedDirectoryEntriesOffset = buffer.byteLength - 4 - 92 * 2;
+    const protocol = buffer.readInt32LE(8);
+    const networkProtocol = buffer.readInt32LE(12);
+    const mapName = readString(buffer, 13, 260);
+    const gameDirectory = readString(buffer, 274, 260);
+    const mapChecksum = buffer.readUInt32LE(535);
 
-  console.assert(
-    directoryEntriesOffset === expectedDirectoryEntriesOffset,
-    `Directory entries offset did not match expected: ${directoryEntriesOffset} !== ${expectedDirectoryEntriesOffset}`
-  );
+    // Validate directory entries offset
+    const directoryEntriesOffset = buffer.readUInt32LE(540);
+    const expectedDirectoryEntriesOffset = buffer.byteLength - 4 - 92 * 2;
 
-  // Validate total directory entries
-  const totalDirectoryEntries = buffer.readInt32LE(directoryEntriesOffset);
+    console.assert(
+      directoryEntriesOffset === expectedDirectoryEntriesOffset,
+      `Directory entries offset did not match expected: ${directoryEntriesOffset} !== ${expectedDirectoryEntriesOffset}`
+    );
 
-  console.assert(
-    totalDirectoryEntries === 2,
-    `Number of directory entries larger than expected: ${totalDirectoryEntries}`
-  );
+    // Validate total directory entries
+    const totalDirectoryEntries = buffer.readInt32LE(directoryEntriesOffset);
 
-  const directoryEntriesStartIndex = directoryEntriesOffset + 4;
+    console.assert(
+      totalDirectoryEntries === 2,
+      `Number of directory entries larger than expected: ${totalDirectoryEntries}`
+    );
 
-  console.log({
-    magic,
-    protocol,
-    networkProtocol,
-    mapName,
-    gameDirectory,
-    mapChecksum,
-    directoryEntries: [
-      readDirectoryEntry(buffer, directoryEntriesStartIndex),
-      readDirectoryEntry(
-        buffer,
-        directoryEntriesStartIndex + DIRECTORY_ENTRY_LENGTH + 1
-      ),
-    ],
-  });
-});
+    const directoryEntriesStartIndex = directoryEntriesOffset + 4;
+
+    console.log({
+      magic,
+      protocol,
+      networkProtocol,
+      mapName,
+      gameDirectory,
+      mapChecksum,
+      directoryEntries: [
+        readDirectoryEntry(buffer, directoryEntriesStartIndex),
+        readDirectoryEntry(
+          buffer,
+          directoryEntriesStartIndex + DIRECTORY_ENTRY_LENGTH + 1
+        ),
+      ],
+    });
+  })
+)();
