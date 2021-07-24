59bcb34ae2de40aeaa97635d849ccde29ccb6e9e
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-07-24 04:02:44 -0400

    Extract NetMsgInfo module

--- packages/core/src/netMsg.ts
+++ packages/core/src/netMsg.ts
@@ -1,10 +1,12 @@
 import { either as E } from "fp-ts";
 import { sequenceS } from "fp-ts/lib/Apply";
 import { pipe } from "fp-ts/lib/function";
-import { float32_le, int32_le, point } from "./parser";
+import type { NetMsgInfo } from "./netMsgInfo";
+import { netMsgInfo } from "./netMsgInfo";
+import { int32_le } from "./parser";
 
 export type NetMsg = {
-  readonly info: unknown;
+  readonly info: NetMsgInfo;
   readonly incomingSequence: number;
   readonly incomingAcknowledged: number;
   readonly incomingReliableAcknowledged: number;
@@ -33,56 +35,3 @@ export const netMsg =
         msg: E.right({}),
       })
     );
-
-// prettier-ignore
-const netMsgInfo =
-  (buffer: Buffer) =>
-  (cursor = 0) =>
-    pipe(
-      sequenceS(E.Applicative)({
-        timestamp:      float32_le  (buffer)(cursor),
-        viewOrigin:     point       (buffer)(cursor + 4),
-        viewAngles:     point       (buffer)(cursor + 4 + 12),
-        forward:        point       (buffer)(cursor + 4 + 12 + 12),
-        right:          point       (buffer)(cursor + 4 + 12 + 12 + 12),
-        up:             point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12),
-        frameTime:      float32_le  (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12),
-        time:           float32_le  (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4),
-        intermission:   int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4),
-        paused:         int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4),
-        spectator:      int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4),
-        onGround:       int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4),
-        waterLevel:     int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4),
-        simVel:         point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4),
-        simOrg:         point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12),
-        viewHeight:     point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12),
-        idealPitch:     float32_le  (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12),
-        cl_viewangles:  point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4),
-        health:         int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12),
-        crosshairAngle: point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4),
-        viewSize:       float32_le  (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12),
-        punchAngle:     point       (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4),
-        maxClients:     int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12),
-        viewEntity:     int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4),
-        playerNum:      int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4),
-        maxEntities:    int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4),
-        demoPlayback:   int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4),
-        hardware:       int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4),
-        smoothing:      int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4),
-        ptrCmd:         int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4),
-        ptrMoveVars:    int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4),
-        viewPort:       viewPort    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4),
-        nextView:       int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 16),
-        onlyClientDraw: int32_le    (buffer)(cursor + 4 + 12 + 12 + 12 + 12 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 12 + 12 + 12 + 4 + 12 + 4 + 12 + 4 + 12 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 16 + 4),
-      })
-    );
-
-const viewPort =
-  (buffer: Buffer) =>
-  (cursor = 0) =>
-    E.sequenceArray([
-      int32_le(buffer)(cursor),
-      int32_le(buffer)(cursor + 4),
-      int32_le(buffer)(cursor + 4 + 4),
-      int32_le(buffer)(cursor + 4 + 4 + 4),
-    ]);
