80d79f91dc81931c9693ca98ca97fe0317f60716
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-08-01 20:55:21 -0400

    Refactor all parsers

--- packages/core/src/parser.ts
+++ packages/core/src/parser.ts
@@ -1,19 +1,19 @@
-import { array as A, either as E, option as O } from "fp-ts";
+import { either as E } from "fp-ts";
 import type { Applicative2 } from "fp-ts/lib/Applicative";
 import { sequenceS } from "fp-ts/lib/Apply";
 import type { Chain2 } from "fp-ts/lib/Chain";
 import { flow, pipe } from "fp-ts/lib/function";
 import type { Functor2 } from "fp-ts/lib/Functor";
 import type { Monad2 } from "fp-ts/lib/Monad";
-import { eq, not } from "./utils";
 
 type Stream<I> = { readonly buffer: I; readonly cursor: number };
 
-export type Parser<I, A> = (
-  stream: Stream<I>
-) => E.Either<Error, ParseResult<I, A>>;
+export type Parser<I, A> = (stream: Stream<I>) => ParseResult<I, A>;
 
-type ParseResult<I, A> = { readonly value: A; readonly next: Stream<I> };
+type ParseResult<I, A> = E.Either<
+  Error,
+  { readonly value: A; readonly next: Stream<I> }
+>;
 
 const URI = "Parser";
 
@@ -48,17 +48,18 @@ const chain_: Chain2<URI>["chain"] = (fa, f) =>
     E.chain(({ value, next }) => f(value)(next))
   );
 
-export const ap: <I, A, B>(
-  fab: Parser<I, (a: A) => B>
-) => (fa: Parser<I, A>) => Parser<I, B> = (fab) => (fa) => ap_(fab, fa);
+export const ap: <I, A>(
+  fa: Parser<I, A>
+) => <B>(fab: Parser<I, (a: A) => B>) => Parser<I, B> = (fa) => (fab) =>
+  ap_(fab, fa);
 
 export const chain: <I, A, B>(
-  fa: Parser<I, A>
-) => (f: (a: A) => Parser<I, B>) => Parser<I, B> = (fa) => (f) => chain_(fa, f);
+  f: (a: A) => Parser<I, B>
+) => (fa: Parser<I, A>) => Parser<I, B> = (f) => (fa) => chain_(fa, f);
 
-export const map: <I, A, B>(
-  fa: Parser<I, A>
-) => (f: (a: A) => B) => Parser<I, B> = (fa) => (f) => map_(fa, f);
+export const map: <A, B>(
+  f: (a: A) => B
+) => <I>(fa: Parser<I, A>) => Parser<I, B> = (f) => (fa) => map_(fa, f);
 
 export const of: <I, A>(a: A) => Parser<I, A> = (a) => (i) =>
   E.right({ value: a, next: i });
@@ -90,82 +91,80 @@ export const Monad: Monad2<URI> = {
   of,
 };
 
+const success: <I, A>(value: A, next: Stream<I>) => ParseResult<I, A> = (
+  value,
+  next
+) => E.right({ value, next });
+
 export type Point = {
   readonly x: number;
   readonly y: number;
   readonly z: number;
 };
 
-export const str =
-  (buffer: Buffer) =>
-  (cursor = 0) =>
-  (length = 1): string =>
-    pipe(
-      A.range(1, length),
-      A.filterMapWithIndex(
-        flow(
-          (i) => cursor + i,
-          char(buffer),
-          O.fromEither,
-          O.chain(O.fromPredicate(flow(eq("\x00"), not)))
-        )
-      ),
-      (a) => a.join("")
-    );
+// TODO Need a manyN combinator
+export const str: (byteLength: number) => Parser<Buffer, string> =
+  (byteLength) => (i) =>
+    success("", { ...i, cursor: i.cursor + byteLength });
 
-export const char =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, string> =>
-    pipe(
-      E.tryCatch(() => buffer.readInt8(cursor), E.toError),
-      E.map(String.fromCharCode)
-    );
+export const char: Parser<Buffer, string> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readInt8(i.cursor), E.toError),
+    E.chain((x) =>
+      success(String.fromCharCode(x), { ...i, cursor: i.cursor + 1 })
+    )
+  );
 
-export const uint32_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readUInt32LE(cursor), E.toError);
+export const uint32_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readUInt32LE(i.cursor), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 4 }))
+  );
 
-export const int32_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readInt32LE(cursor), E.toError);
+export const int32_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readInt32LE(i.cursor), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 4 }))
+  );
 
-export const uint16_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readUInt16LE(cursor), E.toError);
+export const uint16_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readUInt16LE(i.cursor), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 2 }))
+  );
 
-export const int16_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readInt16LE(cursor), E.toError);
+export const int16_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readInt16LE(i.cursor), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 2 }))
+  );
 
-export const uint8_be =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readUIntBE(cursor, 1), E.toError);
+export const uint8_be: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readUIntBE(i.cursor, 1), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 1 }))
+  );
 
-export const int8_be =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readIntBE(cursor, 1), E.toError);
+export const int8_be: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readIntBE(i.cursor, 1), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 1 }))
+  );
 
-export const uint8_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readUIntLE(cursor, 1), E.toError);
+export const uint8_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readUIntLE(i.cursor, 1), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 1 }))
+  );
 
-export const float32_le =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, number> =>
-    E.tryCatch(() => buffer.readFloatLE(cursor), E.toError);
+export const float32_le: Parser<Buffer, number> = (i) =>
+  pipe(
+    E.tryCatch(() => i.buffer.readFloatLE(i.cursor), E.toError),
+    E.chain((x) => success(x, { ...i, cursor: i.cursor + 4 }))
+  );
 
-export const point =
-  (buffer: Buffer) =>
-  (cursor = 0): E.Either<Error, Point> =>
-    sequenceS(E.Applicative)({
-      x: float32_le(buffer)(cursor),
-      y: float32_le(buffer)(cursor + 4),
-      z: float32_le(buffer)(cursor + 4 + 4),
-    });
+export const point: Parser<Buffer, Point> = sequenceS(Applicative)({
+  x: float32_le,
+  y: float32_le,
+  z: float32_le,
+});
