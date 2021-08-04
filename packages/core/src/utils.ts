f2c199a0a882ad8fbd287eaf0c1c5bc26e77856c
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-08-04 17:07:13 -0400

    Workflow (#2)
    * Repurpose point parser for RGB
    
    * Add CI workflow

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,20 +1,9 @@
-import type { io as IO } from "fp-ts";
-import type { InspectOptions } from "util";
-
 export const not = (a: boolean): boolean => !a;
 
 export const eq =
   <A>(a: A) =>
   <B extends A>(b: B): boolean =>
     a === b;
-
-export const dir =
-  (options: InspectOptions) =>
-  (obj: unknown): IO.IO<void> =>
-  () =>
-    console.dir(obj, options);
-
-export const dumpObject = dir({ depth: Infinity });
 
 export const toError =
   <A>(message: string) =>
