9cf80b7319ac9bd27f2339653c14364a83441c1a
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-08-06 00:33:18 -0400

    Update parser tests

--- packages/parser/src/buffer.test.ts
+++ packages/parser/src/buffer.test.ts
@@ -1,55 +1,72 @@
 import * as B from "./buffer";
-import { of as stream } from "./Stream";
 import { failure, success } from "./ParseResult";
+import { of as stream } from "./Stream";
 
-test("str", () => {
-  const buffer = Buffer.from("foo");
+describe("buffer", () => {
+  test.each([
+    [8, 1],
+    [16, 257],
+    [24, 65793],
+    [32, 16843009],
+    [40, 4311810305],
+    [48, 1103823438081],
+  ] as const)("int_le(%s) -> %s", (bitLength, expected) => {
+    const buffer = Buffer.of(1, 1, 1, 1, 1, 1);
 
-  expect(B.str(1)(stream(buffer))).toStrictEqual(
-    success("f", stream(buffer), stream(buffer, 1))
-  );
+    expect(B.int_le(bitLength)(stream(buffer))).toStrictEqual(
+      success(expected, stream(buffer), stream(buffer, bitLength / 8))
+    );
+  });
 
-  expect(B.str(3)(stream(buffer))).toStrictEqual(
-    success("foo", stream(buffer), stream(buffer, 3))
-  );
+  test("str", () => {
+    const buffer = Buffer.from("foo");
 
-  const longerBuffer = Buffer.from("foo bar baz");
+    expect(B.str(1)(stream(buffer))).toStrictEqual(
+      success("f", stream(buffer), stream(buffer, 1))
+    );
 
-  expect(B.str(3)(stream(longerBuffer, 4))).toStrictEqual(
-    success("bar", stream(longerBuffer, 4), stream(longerBuffer, 7))
-  );
-});
+    expect(B.str(3)(stream(buffer))).toStrictEqual(
+      success("foo", stream(buffer), stream(buffer, 3))
+    );
 
-test("char", () => {
-  const buffer = Buffer.from("foo");
+    const longerBuffer = Buffer.from("foo bar baz");
 
-  expect(B.char(stream(buffer))).toStrictEqual(
-    success("f", stream(buffer), stream(buffer, 1))
-  );
+    expect(B.str(3)(stream(longerBuffer, 4))).toStrictEqual(
+      success("bar", stream(longerBuffer, 4), stream(longerBuffer, 7))
+    );
+  });
 
-  expect(B.char(stream(buffer, 1))).toStrictEqual(
-    success("o", stream(buffer, 1), stream(buffer, 2))
-  );
+  test("char", () => {
+    const buffer = Buffer.from("foo");
 
-  expect(B.char(stream(buffer, 2))).toStrictEqual(
-    success("o", stream(buffer, 2), stream(buffer, 3))
-  );
+    expect(B.char(stream(buffer))).toStrictEqual(
+      success("f", stream(buffer), stream(buffer, 1))
+    );
 
-  expect(B.char(stream(buffer, -1))).toStrictEqual(failure("TODO"));
-});
+    expect(B.char(stream(buffer, 1))).toStrictEqual(
+      success("o", stream(buffer, 1), stream(buffer, 2))
+    );
 
-test("uint32_le", () => {
-  const buffer = Buffer.of(0, 0, 0, 1);
+    expect(B.char(stream(buffer, 2))).toStrictEqual(
+      success("o", stream(buffer, 2), stream(buffer, 3))
+    );
 
-  expect(B.uint32_le(stream(buffer))).toStrictEqual(
-    success(16777216, stream(buffer), stream(buffer, 4))
-  );
-});
+    expect(B.char(stream(buffer, -1))).toStrictEqual(failure("TODO"));
+  });
 
-test("int32_le", () => {
-  const buffer = Buffer.of(0, 0, 0, -1);
+  test("uint32_le", () => {
+    const buffer = Buffer.of(0, 0, 0, 1);
 
-  expect(B.int32_le(stream(buffer))).toStrictEqual(
-    success(-16777216, stream(buffer), stream(buffer, 4))
-  );
+    expect(B.uint32_le(stream(buffer))).toStrictEqual(
+      success(16777216, stream(buffer), stream(buffer, 4))
+    );
+  });
+
+  test("int32_le", () => {
+    const buffer = Buffer.of(0, 0, 0, -1);
+
+    expect(B.int32_le(stream(buffer))).toStrictEqual(
+      success(-16777216, stream(buffer), stream(buffer, 4))
+    );
+  });
 });
