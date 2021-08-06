140ab49ae8f376b02cc3f15294b483317c63bc81
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-08-06 16:17:18 -0400

    Add pre-push hook to check entire project

--- packages/parser-goldsrc/src/userCmd.ts
+++ packages/parser-goldsrc/src/userCmd.ts
@@ -2,7 +2,7 @@ import { parser as P } from "@talent/parser";
 import { buffer as B } from "@talent/parser-buffer";
 import { sequenceS } from "fp-ts/lib/Apply";
 import { pipe } from "fp-ts/lib/function";
-import type { Point} from "./Point";
+import type { Point } from "./Point";
 import { point } from "./Point";
 
 export type UserCmd = {
