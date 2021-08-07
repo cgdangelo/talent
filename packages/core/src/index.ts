ec402616d6f041a360412d08e4422ee200a963a4
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-08-07 18:56:25 -0400

    Remove utils module

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,32 +3,34 @@ import type { Stream } from "@talent/parser/lib/Stream";
 import { either as E, string as S, taskEither as TE } from "fp-ts";
 import { constant, flow, pipe } from "fp-ts/lib/function";
 import { readFile } from "fs/promises";
-import { eq, not } from "./utils";
 
-const readFileContents = (path: string): TE.TaskEither<Error, Stream<Buffer>> =>
+const readFileContents: (
+  path: string
+) => TE.TaskEither<string, Stream<Buffer>> = (path) =>
   pipe(
-    TE.tryCatch(() => readFile(path), E.toError),
+    TE.tryCatch(
+      () => readFile(path),
+      flow(E.toError, (a) => a.message)
+    ),
     TE.map((buffer) => ({ buffer, cursor: 0 }))
   );
 
-const validateDemoPath = (path?: string): E.Either<Error, string> =>
-  pipe(
-    path,
-    E.fromNullable(new Error("no demo path provided")),
-    E.map((s) => s.trim()),
-    E.chain(
-      E.fromPredicate(
-        flow(S.size, eq(0), not),
-        constant(new Error("demo path is empty"))
-      )
+const validateDemoPath: (path?: string) => E.Either<string, string> = flow(
+  E.fromNullable("no demo path provided"),
+  E.map((s) => s.trim()),
+  E.chain(
+    E.fromPredicate(
+      flow(S.size, (a) => a !== 0),
+      constant("demo path is empty")
     )
-  );
+  )
+);
 
 const main = pipe(
   validateDemoPath(process.argv[2]),
   TE.fromEither,
   TE.chain(readFileContents),
-  TE.chainEitherKW(demo)
+  TE.chainEitherK(demo)
 );
 
 main()
