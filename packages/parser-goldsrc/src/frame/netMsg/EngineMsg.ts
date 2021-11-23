64f9ad4126c44bd5424f6fff7c7dbfd9fe310637
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-11-23 13:40:03 -0500

    feat: unsafe delta description parsing

--- packages/parser-goldsrc/src/frame/netMsg/EngineMsg.ts
+++ packages/parser-goldsrc/src/frame/netMsg/EngineMsg.ts
@@ -1,3 +1,4 @@
+import * as BB from "@talent/parser-bitbuffer";
 import { buffer as B } from "@talent/parser-buffer";
 import * as P from "@talent/parser/lib/Parser";
 import { success } from "@talent/parser/lib/ParseResult";
@@ -8,7 +9,6 @@ import {
 } from "fp-ts";
 import { flow, pipe } from "fp-ts/lib/function";
 import { stream } from "parser-ts/lib/Stream";
-import * as BB from "@talent/parser-bitbuffer";
 import { point } from "../../Point";
 import { moveVars } from "../MoveVars";
 
@@ -21,7 +21,7 @@ export const engineMsgs: (messageBuffer: Buffer) => B.BufferParser<EngineMsg> =
       stream(messageBuffer as unknown as number[]),
 
       pipe(
-        P.many(engineMsg),
+        P.many(P.logPositions(engineMsg)),
         P.chain((parsedMessages) => () => success(parsedMessages, i, i))
       )
     );
@@ -129,8 +129,175 @@ const engineMsg_: (messageId: Message) => B.BufferParser<unknown> = (
         clientCdKeyHash: P.take(16),
       });
 
-    case Message.SVC_DELTADESCRIPTION: // 14
-      return P.fail();
+    // 14
+    case Message.SVC_DELTADESCRIPTION: {
+      enum DeltaType {
+        DT_BYTE = 1,
+        DT_SHORT = 1 << 1,
+        DT_FLOAT = 1 << 2,
+        DT_INTEGER = 1 << 3,
+        DT_ANGLE = 1 << 4,
+        DT_TIMEWINDOW_8 = 1 << 5,
+        DT_TIMEWINDOW_BIG = 1 << 6,
+        DT_STRING = 1 << 7,
+        DT_SIGNED = 1 << 31,
+      }
+
+      type DeltaDecoder = {
+        readonly name: string;
+        readonly bits: number;
+        readonly divisor: number;
+        readonly flags: DeltaType;
+      }[];
+
+      const deltaDecoders: Map<string, DeltaDecoder> = new Map([
+        [
+          "delta_description_t",
+          [
+            {
+              name: "flags",
+              bits: 32,
+              divisor: 1,
+              flags: DeltaType.DT_INTEGER,
+            },
+            {
+              name: "name",
+              bits: 8,
+              divisor: 1,
+              flags: DeltaType.DT_STRING,
+            },
+            {
+              name: "offset",
+              bits: 16,
+              divisor: 1,
+              flags: DeltaType.DT_INTEGER,
+            },
+            {
+              name: "size",
+              bits: 8,
+              divisor: 1,
+              flags: DeltaType.DT_INTEGER,
+            },
+            {
+              name: "bits",
+              bits: 8,
+              divisor: 1,
+              flags: DeltaType.DT_INTEGER,
+            },
+            {
+              name: "divisor",
+              bits: 32,
+              divisor: 4000,
+              flags: DeltaType.DT_FLOAT,
+            },
+            {
+              name: "preMultiplier",
+              bits: 32,
+              divisor: 4000,
+              flags: DeltaType.DT_FLOAT,
+            },
+          ],
+        ],
+      ]);
+
+      const readField = (
+        index: number,
+        deltaDecoder: DeltaDecoder
+      ): P.Parser<number, { [fieldName: string]: string | number }> => {
+        if (
+          deltaDecoder[index]!.flags &
+          (DeltaType.DT_BYTE |
+            DeltaType.DT_SHORT |
+            DeltaType.DT_INTEGER |
+            DeltaType.DT_FLOAT |
+            DeltaType.DT_TIMEWINDOW_8 |
+            DeltaType.DT_TIMEWINDOW_BIG)
+        ) {
+          if (deltaDecoder[index]!.flags & DeltaType.DT_SIGNED) {
+            return pipe(
+              P.struct({
+                sign: pipe(
+                  BB.bits(1),
+                  P.map((a) => (a ? -1 : 1))
+                ),
+                value: BB.bits(deltaDecoder[index]!.bits - 1),
+                divisor: P.of(deltaDecoder[index]!.divisor),
+              }),
+
+              P.map(({ sign, value, divisor }) => ({
+                [deltaDecoder[index]!.name]: (sign * value) / divisor,
+              }))
+            );
+          } else {
+            return pipe(
+              BB.bits(deltaDecoder[index]!.bits),
+              P.map((value) => ({
+                [deltaDecoder[index]!.name]:
+                  value / deltaDecoder[index]!.divisor,
+              }))
+            );
+          }
+        } else if (deltaDecoder[index]!.flags & DeltaType.DT_ANGLE) {
+          return pipe(
+            BB.bits(deltaDecoder[index]!.bits),
+            P.map((value) => ({
+              [deltaDecoder[index]!.name]:
+                value * (360 / (1 << deltaDecoder[index]!.bits)),
+            }))
+          );
+        } else if (deltaDecoder[index]!.flags & DeltaType.DT_STRING) {
+          return P.struct({ [deltaDecoder[index]!.name]: BB.ztstr });
+        }
+
+        return P.succeed({});
+      };
+
+      const readDelta = (deltaDecoder: DeltaDecoder) =>
+        pipe(
+          BB.bits(3),
+
+          P.chain((maskBitCount) => P.manyN(BB.bits(8), maskBitCount)),
+
+          P.chain((maskBits) => {
+            const fieldParsers: ReturnType<typeof readField>[] = [];
+
+            for (let i = 0; i < maskBits.length; ++i) {
+              for (let j = 0; j < 8; ++j) {
+                const index = j + i * 8;
+
+                if (index === deltaDecoder.length) break;
+
+                if (maskBits[i]! & (1 << j)) {
+                  fieldParsers.push(readField(index, deltaDecoder));
+                }
+              }
+            }
+
+            return pipe(
+              RA.sequence(P.Applicative)(fieldParsers),
+              P.map(RA.reduce({}, (acc, cur) => ({ ...acc, ...cur })))
+            );
+          })
+        );
+
+      return pipe(
+        P.struct({ name: B.ztstr, fieldCount: B.uint16_le, fields: P.of([]) }),
+
+        P.chain(
+          (delta) => (i) =>
+            pipe(
+              stream(i.buffer, i.cursor * 8),
+              pipe(
+                P.manyN(
+                  readDelta(deltaDecoders.get("delta_description_t")!),
+                  delta.fieldCount
+                ),
+                P.map((fields) => ({ ...delta, fields }))
+              )
+            )
+        )
+      );
+    }
 
     case Message.SVC_CLIENTDATA: // 15
       return P.fail();
