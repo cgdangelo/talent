491459ee49673bcbe103cd8f1257708cbd1b889f
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-11-29 14:47:58 -0500

    fix: skip bits in SVC_CLIENTDATA

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_CLIENTDATA.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_CLIENTDATA.ts
@@ -12,72 +12,52 @@ export type ClientData = {
   readonly weaponData: readonly Delta[];
 };
 
-// FIXME Something is wrong in this parser? Possibly wrong size because an
-// SVC_BAD is following the first instance of this message in demo.dem. hlviewer
-// misses out on that message because it skips to the end of network message
-// data when parsing a message without fields (SVC_NOP, SVC_CHOKE).
-//
-// 2021-11-29
-// 22 missing somewhere (hlviewer @ 49619, talent @ 49597)
-//
-// talent:   {"clientData":{"flTimeStepSound":258,"origin[1]":-32,"origin[2]":137.984375,"velocity[0]":236.75,"weaponanim":0,"maxspeed":3283.1,"flDuckTime":0},"weaponData":[]}}
-// hlviewer: {"clientData":{"origin[0]":639,"origin[1]":-947,"origin[2]":126,"m_flNextAttack":-0.001,"flags":8,"health":100,"maxspeed":350,"view_ofs[2]":22,"deadflag":2,"physinfo":"\\slj\\0\\hl\\1","fuser4":100}}
-//
-// - bad delta reader, bad delta description?
-// - m_flNextAttack = 22 bits
-// - clientdata_t delta descriptions are identical
-// - mask bits when reading clientdata_t do not align
-//
-// talent:   [ 29, 208 ]
-// hlviewer: [ 142, 104, 129, 2, 64 ]
-//
-// 2021-11-29
-// - need to skip a bit if no delta mask found
+const deltaUpdateMask = pipe(
+  BB.ubits(1),
+
+  P.chain((hasDeltaMask) =>
+    pipe(
+      P.of<number, number>(hasDeltaMask),
+      P.filter((a) => a !== 0),
+      P.apSecond(BB.ubits(8)),
+      P.alt(() => P.of<number, number | undefined>(undefined))
+    )
+  )
+
+  // TODO alternatively
+  // P.chain((hasDeltaMask) =>
+  //   hasDeltaMask !== 0 ? BB.ubits(8) : P.of(undefined)
+  // )
+);
+
+const weaponData = pipe(
+  P.many(
+    pipe(
+      BB.ubits(1),
+      P.filter((hasWeaponData) => hasWeaponData !== 0),
+      P.apSecond(
+        P.struct({
+          weaponIndex: BB.ubits(6),
+          weaponData: readDelta("weapon_data_t"),
+        })
+      ),
+      P.map(({ weaponIndex, weaponData }) => ({ weaponIndex, ...weaponData }))
+    )
+  ),
+
+  P.apFirst(P.skip(1)) // skip hasWeaponData flag
+);
+
 export const clientData: B.BufferParser<ClientData> = (i) =>
   pipe(
     stream(i.buffer, i.cursor * 8),
 
     pipe(
-      BB.ubits(1),
-      P.filter((a) => a !== 0),
-      P.apSecond(BB.ubits(8)),
-      P.bindTo("deltaUpdateMask"),
-      P.alt(() =>
-        pipe(
-          P.of<number, { deltaUpdateMask?: number }>({}),
-          P.apFirst(P.skip<number>(1))
-        )
-      ),
-
-      P.chain((delta) =>
-        pipe(
-          readDelta("clientdata_t"),
-          P.map((clientData) => ({ ...delta, clientData }))
-        )
-      ),
-
-      P.chain((delta) =>
-        pipe(
-          P.many(
-            pipe(
-              BB.ubits(1),
-              P.filter((hasWeaponData) => hasWeaponData !== 0),
-              P.apSecond(
-                P.struct({
-                  weaponIndex: BB.ubits(6),
-                  weaponData: readDelta("weapon_data_t"),
-                })
-              ),
-              P.map(({ weaponIndex, weaponData }) => ({
-                weaponIndex,
-                ...weaponData,
-              }))
-            )
-          ),
-          P.map((weaponData) => ({ ...delta, weaponData }))
-        )
-      ),
-
+      P.struct({
+        deltaUpdateMask,
+        clientData: readDelta("clientdata_t"),
+        weaponData,
+      }),
       BB.nextByte
     )
   );
