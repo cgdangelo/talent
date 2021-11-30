4651877d2e2e6915224b9cc9bec881899ee8d2f7
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-11-29 19:38:41 -0500

    fix: svc_packetentities

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_PACKETENTITIES.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_PACKETENTITIES.ts
@@ -6,10 +6,93 @@ import { pipe } from "fp-ts/lib/function";
 import type { Delta } from "../../../delta";
 import { readDelta } from "../../../delta";
 
-export type PacketEntities = {
+type PacketEntity = {
   readonly entityIndex: number;
+  readonly baselineIndex?: number;
   readonly entityState: Delta;
-}[];
+};
+
+export type PacketEntities = {
+  readonly entityCount: number;
+  readonly entityStates: readonly PacketEntity[];
+};
+
+const entityStates: B.BufferParser<PacketEntities["entityStates"]> = (() => {
+  let entityIndex = 0;
+
+  return pipe(
+    P.many(
+      pipe(
+        BB.ubits(16),
+        P.filter((footer) => footer !== 0),
+        P.apSecond(P.skip(-16)),
+
+        P.apSecond(
+          pipe(
+            BB.ubits(1),
+
+            P.chain((incrementEntityIndex) =>
+              incrementEntityIndex !== 0
+                ? P.of(1)
+                : pipe(
+                    BB.ubits(1),
+                    P.chain((absoluteEntityIndex) =>
+                      absoluteEntityIndex !== 0
+                        ? pipe(
+                            BB.ubits(11),
+                            P.map(
+                              (nextEntityIndex) => nextEntityIndex - entityIndex
+                            )
+                          )
+                        : BB.ubits(6)
+                    )
+                  )
+            ),
+            P.map((entityIndexDiff) => (entityIndex += entityIndexDiff))
+          )
+        ),
+
+        P.apSecond(
+          pipe(
+            P.struct({
+              hasCustomDelta: BB.ubits(1),
+              baselineIndex: pipe(
+                BB.ubits(1),
+                P.chain((hasBaselineIndex) =>
+                  hasBaselineIndex !== 0
+                    ? BB.ubits(6)
+                    : P.of<number, number | undefined>(undefined)
+                )
+              ),
+            })
+          )
+        ),
+
+        P.chain(({ hasCustomDelta, baselineIndex }) =>
+          pipe(
+            P.logPositions(
+              readDelta(
+                entityIndex > 0 && entityIndex < 33
+                  ? "entity_state_player_t"
+                  : hasCustomDelta !== 0
+                  ? "custom_entity_state_t"
+                  : "entity_state_t"
+              )
+            ),
+
+            P.map((entityState) => ({
+              entityIndex,
+              baselineIndex,
+              entityState,
+            }))
+          )
+        )
+      )
+    ),
+
+    P.apFirst(P.skip(16))
+  );
+})();
 
 // TODO Refactor this + SVC_DELTAPACKETENTITIES
 export const packetEntities: B.BufferParser<PacketEntities> = (i) =>
@@ -17,54 +100,10 @@ export const packetEntities: B.BufferParser<PacketEntities> = (i) =>
     stream(i.buffer, i.cursor * 8),
 
     pipe(
-      P.skip<number>(16),
-      P.apSecond(
-        P.many(
-          (() => {
-            let entityIndex = 0;
-
-            return pipe(
-              BB.bits(16),
-              P.filter((footer) => footer !== 0),
-              P.apSecond(
-                P.struct({
-                  removeEntity: BB.ubits(1),
-                  absoluteEntityNumber: BB.ubits(1),
-                })
-              ),
-              P.chainFirst((a) =>
-                pipe(
-                  BB.ubits(a.absoluteEntityNumber ? 11 : 6),
-                  P.map(
-                    (entIdx) =>
-                      (entityIndex = a.absoluteEntityNumber
-                        ? entIdx
-                        : entityIndex + entIdx)
-                  )
-                )
-              ),
-              P.chain((a) =>
-                a.removeEntity === 0 ? P.succeed(a) : P.fail<number>()
-              ),
-
-              P.apSecond(
-                pipe(
-                  BB.bits(1),
-                  P.map((custom) =>
-                    entityIndex > 0 && entityIndex < 33
-                      ? "entity_state_player_t"
-                      : custom !== 0
-                      ? "custom_entity_state_t"
-                      : "entity_state_t"
-                  )
-                )
-              ),
-              P.chain(readDelta),
-              P.map((entityState) => ({ entityIndex, entityState })),
-              BB.nextByte
-            );
-          })()
-        )
-      )
+      P.struct({
+        entityCount: BB.ubits(16),
+        entityStates,
+      }),
+      BB.nextByte
     )
   );
