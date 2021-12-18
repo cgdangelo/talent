921e337d941ae3c583f8e9bba5e6eec442c359ff
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-12-18 13:59:16 -0500

    refactor(svc_crosshairangle): struct

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_CROSSHAIRANGLE.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_CROSSHAIRANGLE.ts
@@ -1,16 +1,12 @@
 import { buffer as B } from "@talent/parser-buffer";
 import * as P from "@talent/parser/lib/Parser";
-import { pipe } from "fp-ts/lib/function";
 
 export type CrosshairAngle = {
   readonly pitch: number;
   readonly yaw: number;
 };
 
-export const crosshairAngle: B.BufferParser<CrosshairAngle> =
-  // TODO hlviewer does not scale these. Find out what "engine call" means
-  // here: https://wiki.alliedmods.net/Half-Life_1_Engine_Messages#SVC_CROSSHAIRANGLE
-  pipe(
-    P.tuple(B.int16_le, B.int16_le),
-    P.map(([pitch, yaw]) => ({ pitch, yaw }))
-  );
+export const crosshairAngle: B.BufferParser<CrosshairAngle> = P.struct({
+  pitch: B.int16_le,
+  yaw: B.int16_le,
+});
