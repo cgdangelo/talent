51ad38a0798f34d57a20e0665a353238e19d8f4a
Charles D'Angelo <c.g.dangelo@gmail.com>
2021-12-19 01:26:58 -0500

    refactor(svc_event): bind

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_EVENT.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_EVENT.ts
@@ -15,61 +15,44 @@ export type Event = {
   }[];
 };
 
+const packetIndex: B.BufferParser<Event["events"][number]["packetIndex"]> =
+  pipe(
+    BB.ubits(1),
+    P.chain((hasPacketIndex) =>
+      hasPacketIndex !== 0 ? BB.ubits(11) : P.of(undefined)
+    )
+  );
+
+const delta: B.BufferParser<Event["events"][number]["delta"]> = pipe(
+  BB.ubits(1),
+  P.chain((hasDelta) =>
+    hasDelta !== 0 ? readDelta("event_t") : P.of(undefined)
+  )
+);
+
+const fireTime: B.BufferParser<Event["events"][number]["fireTime"]> = pipe(
+  BB.ubits(1),
+  P.chain((hasFireTime) => (hasFireTime !== 0 ? BB.ubits(16) : P.of(undefined)))
+);
+
 const events: (eventCount: number) => B.BufferParser<Event["events"]> = (
   eventCount
 ) =>
   pipe(
-    P.struct({ eventIndex: BB.ubits(10) }),
+    P.struct({ eventIndex: BB.ubits(1), packetIndex }),
 
-    P.chain((event) =>
-      pipe(
-        BB.ubits(1),
-        P.chain((hasPacketIndex) =>
-          hasPacketIndex !== 0
-            ? pipe(
-                BB.ubits(11),
-                P.chain((packetIndex) =>
-                  pipe(
-                    BB.ubits(1),
-                    P.chain((hasDelta) =>
-                      hasDelta !== 0 ? readDelta("event_t") : P.of(undefined)
-                    ),
-                    P.map((delta) => ({ packetIndex, delta }))
-                  )
-                )
-              )
-            : P.of({})
-        ),
-        P.map((a) => ({ ...event, ...a }))
-      )
+    P.bind("delta", ({ packetIndex }) =>
+      packetIndex != null ? delta : P.of(undefined)
     ),
 
-    P.chain((event) =>
-      pipe(
-        BB.ubits(1),
-        P.chain((hasFireTime) =>
-          hasFireTime !== 0
-            ? pipe(
-                BB.ubits(16),
-                P.map((fireTime) => ({ fireTime }))
-              )
-            : P.of({})
-        ),
-        P.map((a) => ({ ...event, ...a }))
-      )
-    ),
+    P.bind("fireTime", () => fireTime),
 
-    (fa) => P.manyN(fa, eventCount)
+    (event) => P.manyN(event, eventCount)
   );
 
 export const event: B.BufferParser<Event> = (i) =>
   pipe(
     stream(i.buffer, i.cursor * 8),
 
-    pipe(
-      BB.ubits(5),
-      P.chain(events),
-      P.map((events) => ({ events })),
-      BB.nextByte
-    )
+    pipe(BB.ubits(5), P.chain(events), P.bindTo("events"), BB.nextByte)
   );
