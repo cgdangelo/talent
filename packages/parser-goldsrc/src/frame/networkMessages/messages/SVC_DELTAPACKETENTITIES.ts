1e471e730a742da1fd9c59725cb9f9e7b56352ad
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-17 15:41:45 -0500

    feat: stateful svc_deltapacketentities

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_DELTAPACKETENTITIES.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_DELTAPACKETENTITIES.ts
@@ -1,10 +1,12 @@
+import { parser as P, statefulParser as SP } from "@talent/parser";
 import * as BB from "@talent/parser-bitbuffer";
 import type { buffer as B } from "@talent/parser-buffer";
-import * as P from "@talent/parser/lib/Parser";
+import { success } from "@talent/parser/lib/ParseResult";
 import { stream } from "@talent/parser/lib/Stream";
 import { pipe } from "fp-ts/lib/function";
 import type { Delta } from "../../../delta";
 import { readDelta } from "../../../delta";
+import type { DemoState, DemoStateParser } from "../../../DemoState";
 
 type DeltaPacketEntity = {
   readonly entityIndex: number;
@@ -17,12 +19,12 @@ export type DeltaPacketEntities = {
   readonly entityStates: readonly DeltaPacketEntity[];
 };
 
-const entityState: (entityIndex: number) => B.BufferParser<DeltaPacketEntity> =
+const entityState: (entityIndex: number) => DemoStateParser<DeltaPacketEntity> =
   (entityIndex) =>
     pipe(
-      BB.ubits(1),
+      SP.lift<number, number, DemoState>(BB.ubits(1)),
 
-      P.chain((hasCustomDelta) =>
+      SP.chain((hasCustomDelta) =>
         pipe(
           readDelta(
             entityIndex > 0 && entityIndex < 33
@@ -32,7 +34,7 @@ const entityState: (entityIndex: number) => B.BufferParser<DeltaPacketEntity> =
               : "entity_state_t"
           ),
 
-          P.map((entityState) => ({ entityIndex, entityState }))
+          SP.map((entityState) => ({ entityIndex, entityState }))
         )
       )
     );
@@ -57,31 +59,38 @@ const nextEntityIndex: () => B.BufferParser<number> = () => {
   );
 };
 
-const entityStates: () => B.BufferParser<DeltaPacketEntities["entityStates"]> =
+const entityStates: () => DemoStateParser<DeltaPacketEntities["entityStates"]> =
   () =>
-    P.many(
+    SP.many(
       pipe(
-        // Check footer before continuing
-        P.lookAhead(
+        SP.lift<
+          number,
+          { removeEntity: number; entityIndex: number },
+          DemoState
+        >(
           pipe(
-            BB.ubits(16),
-            P.filter((footer) => footer !== 0)
+            // Check footer before continuing
+            P.lookAhead(
+              pipe(
+                BB.ubits(16),
+                P.filter((footer) => footer !== 0)
+              )
+            ),
+
+            P.chain(() =>
+              P.struct({
+                removeEntity: BB.ubits(1),
+                entityIndex: nextEntityIndex(),
+              })
+            )
           )
         ),
 
-        // Parse entity index
-        P.apSecond(
-          P.struct({
-            removeEntity: BB.ubits(1),
-            entityIndex: nextEntityIndex(),
-          })
-        ),
-
         // Parse entity with the given index
-        P.chain(({ removeEntity, entityIndex }) =>
+        SP.chain(({ removeEntity, entityIndex }) =>
           pipe(
             removeEntity !== 0
-              ? P.of({ entityIndex, entityState: null })
+              ? SP.of({ entityIndex, entityState: null })
               : entityState(entityIndex)
           )
         )
@@ -89,18 +98,28 @@ const entityStates: () => B.BufferParser<DeltaPacketEntities["entityStates"]> =
     );
 
 // TODO Refactor this + SVC_DELTAPACKETENTITIES
-export const deltaPacketEntities: B.BufferParser<DeltaPacketEntities> = (i) =>
-  pipe(
-    stream(i.buffer, i.cursor * 8),
-
+export const deltaPacketEntities: DemoStateParser<DeltaPacketEntities> =
+  (s) => (i) =>
     pipe(
-      P.struct({
-        entityCount: BB.ubits(16),
-        deltaSequence: BB.ubits(8),
-        entityStates: entityStates(),
-      }),
+      stream(i.buffer, i.cursor * 8),
 
-      P.apFirst(P.skip(16)),
-      BB.nextByte
-    )
-  );
+      pipe(
+        SP.lift<number, number, DemoState>(BB.ubits(16)),
+        SP.bindTo("entityCount"),
+        SP.bind("deltaSequence", () => SP.lift(BB.ubits(8))),
+        SP.bind("entityStates", () => entityStates()),
+        SP.chainFirst(() => SP.lift(P.skip(16))),
+        SP.chain((a) =>
+          SP.lift((i) =>
+            success(
+              a,
+              i,
+              stream(
+                i.buffer,
+                i.cursor % 8 === 0 ? i.cursor / 8 : Math.floor(i.cursor / 8) + 1
+              )
+            )
+          )
+        )
+      )(s)
+    );
