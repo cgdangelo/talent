59f19199f770412523a82fc333a5439a6a5cf3f1
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-17 15:20:00 -0500

    feat: stateful svc_event_reliable

--- packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_EVENT_RELIABLE.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/messages/SVC_EVENT_RELIABLE.ts
@@ -1,10 +1,11 @@
+import { statefulParser as SP } from "@talent/parser";
 import * as BB from "@talent/parser-bitbuffer";
-import type { buffer as B } from "@talent/parser-buffer";
-import * as P from "@talent/parser/lib/Parser";
+import { success } from "@talent/parser/lib/ParseResult";
 import { stream } from "@talent/parser/lib/Stream";
 import { pipe } from "fp-ts/lib/function";
 import type { Delta } from "../../../delta";
 import { readDelta } from "../../../delta";
+import type { DemoState, DemoStateParser } from "../../../DemoState";
 
 export type EventReliable = {
   readonly eventIndex: number;
@@ -12,16 +13,29 @@ export type EventReliable = {
   readonly fireTime?: number;
 };
 
-export const eventReliable: B.BufferParser<EventReliable> = (i) =>
+const fireTime = BB.bitFlagged(() => BB.ubits(16));
+
+export const eventReliable: DemoStateParser<EventReliable> = (s) => (i) =>
   pipe(
     stream(i.buffer, i.cursor * 8),
 
     pipe(
-      P.struct({
-        eventIndex: BB.ubits(10),
-        eventArgs: readDelta("event_args_t"),
-        fireTime: BB.bitFlagged(() => BB.ubits(16)),
-      }),
-      BB.nextByte
-    )
+      SP.lift<number, number, DemoState>(BB.ubits(10)),
+      SP.bindTo("eventIndex"),
+      SP.bind("eventArgs", () => readDelta("event_args_t")),
+      SP.bind("fireTime", () => SP.lift(fireTime)),
+
+      SP.chain((a) =>
+        SP.lift((i) =>
+          success(
+            a,
+            i,
+            stream(
+              i.buffer,
+              i.cursor % 8 === 0 ? i.cursor / 8 : Math.floor(i.cursor / 8) + 1
+            )
+          )
+        )
+      )
+    )(s)
   );
