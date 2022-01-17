7235956016371e065158e662af851df24ec9bf67
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-17 14:03:07 -0500

    feat: save delta decoders to state

--- packages/parser-goldsrc/src/DemoState.ts
+++ packages/parser-goldsrc/src/DemoState.ts
@@ -1,11 +1,15 @@
 import type { StatefulParser } from "@talent/parser/lib/StatefulParser";
+import { readonlyMap as RM } from "fp-ts";
+import type { DeltaDecoder } from "./delta";
 
 export type DemoStateParser<A> = StatefulParser<DemoState, number, A>;
 
 export type DemoState = {
+  readonly deltaDecoders: ReadonlyMap<string, DeltaDecoder>;
   readonly networkProtocol: number;
 };
 
 export const initialState: DemoState = {
+  deltaDecoders: RM.empty,
   networkProtocol: 0,
 };
