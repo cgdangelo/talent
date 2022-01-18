5a94032c58930ca1b4c39ac529acdeae51fb78d2
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-18 10:59:56 -0500

    feat(parser): explicit typing

--- packages/parser/src/StatefulParser.ts
+++ packages/parser/src/StatefulParser.ts
@@ -40,11 +40,11 @@ export const fail: <S, I, A = never>() => StatefulParser<S, I, A> = () =>
 export const get: <I, S>() => StatefulParser<S, I, S> = () => (s) =>
   P.of([s, s]);
 
-export const modify: <S, I>(fs: (s: S) => S) => StatefulParser<S, I, void> =
-  (fs) => (s) =>
-    P.of([undefined, fs(s)]);
+export const modify: <S, I>(f: (s: S) => S) => StatefulParser<S, I, void> =
+  (f) => (s) =>
+    P.of([undefined, f(s)]);
 
-export const put: <S, I>(s: S) => StatefulParser<S, I, undefined> = (s) => () =>
+export const put: <S, I>(s: S) => StatefulParser<S, I, void> = (s) => () =>
   P.of([undefined, s]);
 
 export const succeed: <S, I, A>(a: A) => StatefulParser<S, I, A> = (a) =>
@@ -127,45 +127,37 @@ export const manyTill = <S, I, A, B>(
     alt<S, I, ReadonlyArray<A>>(() => many1Till(parser, terminator))
   );
 
-// NOTE Not stack safe
-export const manyN: <S, I, A>(
-  ma: StatefulParser<S, I, A>,
+export const manyN = <S, I, A>(
+  parser: StatefulParser<S, I, A>,
   n: number
-) => StatefulParser<S, I, readonly A[]> = (ma, n) =>
-  pipe(RA.replicate(n, ma), RA.sequence(Applicative));
+): StatefulParser<S, I, readonly A[]> =>
+  n === 0
+    ? of([])
+    : pipe(
+        parser,
 
-// FIXME returns first outgoing position instead of last
-// export const manyN = <S, I, A>(
-//   parser: StatefulParser<S, I, A>,
-//   n: number
-// ): StatefulParser<S, I, readonly A[]> =>
-//   n === 0
-//     ? of([])
-//     : pipe(
-//         parser,
+        chain((x) =>
+          chainRec_(RNEA.of(x), (acc) =>
+            pipe(
+              lift<I, E.Either<never, RNEA.ReadonlyNonEmptyArray<A>>, S>(
+                pipe(
+                  acc.length === n ? P.succeed(undefined) : P.fail<I>(),
+                  P.map(() => E.right(acc))
+                )
+              ),
 
-//         chain((x) =>
-//           chainRec_(RNEA.of(x), (acc) =>
-//             pipe(
-//               lift<I, E.Either<never, RNEA.ReadonlyNonEmptyArray<A>>, S>(
-//                 pipe(
-//                   acc.length === n ? P.succeed(undefined) : P.fail<I>(),
-//                   P.map(() => E.right(acc))
-//                 )
-//               ),
-
-//               alt(() =>
-//                 log(
-//                   pipe(
-//                     parser,
-//                     map((a) => E.left(RA.append(a)(acc)))
-//                   )
-//                 )
-//               )
-//             )
-//           )
-//         )
-//       );
+              alt(() =>
+                log(
+                  pipe(
+                    parser,
+                    map((a) => E.left(RA.append(a)(acc)))
+                  )
+                )
+              )
+            )
+          )
+        )
+      );
 
 // -----------------------------------------------------------------------------
 // non-pipeables
@@ -186,7 +178,11 @@ const alt_: Alt3<URI>["alt"] = (fa, that) => (s) => (i) => {
 
 const ap_: Monad3<URI>["ap"] = (fab, fa) => pipe(fab, ap(fa));
 
-const chain_: Chain3<URI>["chain"] = (fa, f) => pipe(fa, chain(f));
+const chain_: Chain3<URI>["chain"] = (fa, f) =>
+  pipe(
+    fa,
+    chain((a) => f(a))
+  );
 
 const chainRec_: ChainRec3<URI>["chainRec"] = <S, I, A, B>(
   a: A,
@@ -237,9 +233,16 @@ export const alt: <S, I, A>(
   (that) => (ma) =>
     alt_(ma, that);
 
-export const ap = ST.ap(P.Monad);
+export const ap: <S, I, A>(
+  ma: StatefulParser<S, I, A>
+) => <B>(mab: StatefulParser<S, I, (a: A) => B>) => StatefulParser<S, I, B> =
+  ST.ap(P.Monad);
 
-export const chain = ST.chain(P.Monad);
+export const chain: <A, S, I, B>(
+  f: (a: A) => StatefulParser<S, I, B>
+) => (ma: StatefulParser<S, I, A>) => StatefulParser<S, I, B> = ST.chain(
+  P.Monad
+);
 
 export const chainFirst: <A, S, I, B>(
   f: (a: A) => StatefulParser<S, I, B>
@@ -254,9 +257,13 @@ export const chainFirst: <A, S, I, B>(
     )
   );
 
-export const map = ST.map(P.Functor);
+export const map: <A, B>(
+  f: (a: A) => B
+) => <S, I>(ma: StatefulParser<S, I, A>) => StatefulParser<S, I, B> = ST.map(
+  P.Functor
+);
 
-export const of = ST.of(P.Monad);
+export const of: <A, S, I>(a: A) => StatefulParser<S, I, A> = ST.of(P.Monad);
 
 // -----------------------------------------------------------------------------
 // instances
@@ -326,11 +333,20 @@ export const Monad: Monad3<URI> = {
 // utils
 // -----------------------------------------------------------------------------
 
-export const evaluate = ST.evaluate(P.Functor);
+export const evaluate: <S>(
+  s: S
+) => <I, A>(ma: StatefulParser<S, I, A>) => P.Parser<I, A> = ST.evaluate(
+  P.Functor
+);
 
-export const execute = ST.execute(P.Functor);
+export const execute: <S>(
+  s: S
+) => <I, A>(ma: StatefulParser<S, I, A>) => P.Parser<I, S> = ST.execute(
+  P.Functor
+);
 
-export const lift = ST.fromF(P.Functor);
+export const lift: <I, A, S>(p: P.Parser<I, A>) => StatefulParser<S, I, A> =
+  ST.fromF(P.Functor);
 
 export const log: <S, I, A>(
   ma: StatefulParser<S, I, A>
@@ -339,11 +355,9 @@ export const log: <S, I, A>(
     ma(s)(i),
     E.map((a) => {
       console.log(
-        `result: ${
-          typeof a.value === "object" && a.value != null
-            ? JSON.stringify(a.value)
-            : a.value
-        }, before: ${i.cursor}, after: ${a.next.cursor}`
+        `result: ${JSON.stringify(a.value[0])}, before: ${i.cursor}, after: ${
+          a.next.cursor
+        }`
       );
 
       return a;
