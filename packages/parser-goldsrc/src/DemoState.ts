0ee7f231b4f182c6d6e34dda56daab0f20ecd2a4
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-18 02:35:58 -0500

    feat: user messages in parser state

--- packages/parser-goldsrc/src/DemoState.ts
+++ packages/parser-goldsrc/src/DemoState.ts
@@ -2,15 +2,18 @@ import type { StatefulParser } from "@talent/parser/lib/StatefulParser";
 import { readonlyMap as RM } from "fp-ts";
 import type { DeltaDecoder } from "./delta";
 import { initialDeltaDecoders } from "./delta";
+import type { NewUserMsg } from "./frame/networkMessages/messages";
 
 export type DemoStateParser<A> = StatefulParser<DemoState, number, A>;
 
 export type DemoState = {
   readonly deltaDecoders: ReadonlyMap<string, DeltaDecoder>;
   readonly networkProtocol: number;
+  readonly userMessages: ReadonlyMap<number, NewUserMsg>;
 };
 
 export const initialState: DemoState = {
   deltaDecoders: RM.fromMap(initialDeltaDecoders),
   networkProtocol: 0,
+  userMessages: RM.empty,
 };
