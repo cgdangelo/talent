e5eeb1ac440668687bea27feeb236c7231fcd510
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-25 13:55:39 -0500

    feat(parser-goldsrc): discriminator fields for frames

--- packages/parser-goldsrc/src/frame/ConsoleCommand.ts
+++ packages/parser-goldsrc/src/frame/ConsoleCommand.ts
@@ -1,6 +1,20 @@
+import { parser as P } from "@talent/parser";
 import { buffer as B } from "@talent/parser-buffer";
+import { pipe } from "fp-ts/lib/function";
+import type { FrameHeader } from "./FrameHeader";
+import { frameHeader } from "./FrameHeader";
 
-export type ConsoleCommand = string;
+export type ConsoleCommand = {
+  readonly header: FrameHeader;
+  readonly type: "ConsoleCommand";
+  readonly frameData: string;
+};
 
-export const consoleCommand: B.BufferParser<ConsoleCommand> =
-  B.ztstr_padded(64);
+export const consoleCommand: B.BufferParser<ConsoleCommand> = pipe(
+  frameHeader,
+  P.bindTo("header"),
+
+  P.bind("type", () => P.of("ConsoleCommand" as const)),
+
+  P.bind("frameData", () => B.ztstr_padded(64))
+);
--- packages/parser-goldsrc/src/frame/networkMessages/NetworkMessages.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/NetworkMessages.ts
@@ -5,6 +5,8 @@ import { pipe } from "fp-ts/lib/function";
 import type { DemoState, DemoStateParser } from "../../DemoState";
 import type { Point } from "../../Point";
 import { point } from "../../Point";
+import type { FrameHeader } from "../FrameHeader";
+import { frameHeader } from "../FrameHeader";
 import type { MoveVars } from "../MoveVars";
 import { moveVars } from "../MoveVars";
 import type { RefParams } from "../RefParams";
@@ -15,20 +17,24 @@ import type { Message } from "./Message";
 import { messages } from "./Message";
 
 export type NetworkMessages = {
-  readonly timestamp: number;
-  readonly refParams: RefParams;
-  readonly userCmd: UserCmd;
-  readonly moveVars: MoveVars;
-  readonly view: Point;
-  readonly viewModel: number;
-  readonly incomingSequence: number;
-  readonly incomingAcknowledged: number;
-  readonly incomingReliableAcknowledged: number;
-  readonly incomingReliableSequence: number;
-  readonly outgoingSequence: number;
-  readonly reliableSequence: number;
-  readonly lastReliableSequence: number;
-  readonly messages: readonly Message[];
+  readonly header: FrameHeader;
+  readonly type: "NetworkMessages";
+  readonly frameData: {
+    readonly timestamp: number;
+    readonly refParams: RefParams;
+    readonly userCmd: UserCmd;
+    readonly moveVars: MoveVars;
+    readonly view: Point;
+    readonly viewModel: number;
+    readonly incomingSequence: number;
+    readonly incomingAcknowledged: number;
+    readonly incomingReliableAcknowledged: number;
+    readonly incomingReliableSequence: number;
+    readonly outgoingSequence: number;
+    readonly reliableSequence: number;
+    readonly lastReliableSequence: number;
+    readonly messages: readonly Message[];
+  };
 };
 
 const messagesLength: B.BufferParser<number> = P.expected(
@@ -40,25 +46,37 @@ const messagesLength: B.BufferParser<number> = P.expected(
 );
 
 export const networkMessages: DemoStateParser<NetworkMessages> = pipe(
-  SP.lift<number, Omit<NetworkMessages, "messages">, DemoState>(
-    P.struct({
-      timestamp: B.float32_le,
-      refParams,
-      userCmd,
-      moveVars,
-      view: point,
-      viewModel: B.int32_le,
-      incomingSequence: B.int32_le,
-      incomingAcknowledged: B.int32_le,
-      incomingReliableAcknowledged: B.int32_le,
-      incomingReliableSequence: B.int32_le,
-      outgoingSequence: B.int32_le,
-      reliableSequence: B.int32_le,
-      lastReliableSequence: B.int32_le,
-    })
-  ),
+  SP.lift(frameHeader) as DemoStateParser<FrameHeader>,
+  SP.bindTo("header"),
 
-  SP.bind("messages", () =>
-    pipe(SP.lift<number, number, DemoState>(messagesLength), SP.chain(messages))
+  SP.bind("type", () => SP.of("NetworkMessages" as const)),
+
+  SP.bind("frameData", () =>
+    pipe(
+      SP.lift(
+        P.struct({
+          timestamp: B.float32_le,
+          refParams,
+          userCmd,
+          moveVars,
+          view: point,
+          viewModel: B.int32_le,
+          incomingSequence: B.int32_le,
+          incomingAcknowledged: B.int32_le,
+          incomingReliableAcknowledged: B.int32_le,
+          incomingReliableSequence: B.int32_le,
+          outgoingSequence: B.int32_le,
+          reliableSequence: B.int32_le,
+          lastReliableSequence: B.int32_le,
+        })
+      ) as DemoStateParser<Omit<NetworkMessages["frameData"], "messages">>,
+
+      SP.bind("messages", () =>
+        pipe(
+          SP.lift<number, number, DemoState>(messagesLength),
+          SP.chain(messages)
+        )
+      )
+    )
   )
 );
