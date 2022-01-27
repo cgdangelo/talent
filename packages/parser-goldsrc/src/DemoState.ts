bc55ba8af7035a61c74b3bbc85208ce5457e4ba0
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-27 10:52:19 -0500

    feat(parser-goldsrc): add demo state lifter

--- packages/parser-goldsrc/src/DemoState.ts
+++ packages/parser-goldsrc/src/DemoState.ts
@@ -1,10 +1,13 @@
-import type { StatefulParser } from "@talent/parser/lib/StatefulParser";
+import type { parser as P } from "@talent/parser";
+import { statefulParser as SP } from "@talent/parser";
 import { readonlyMap as RM } from "fp-ts";
 import type { DeltaDecoder } from "./delta";
 import { initialDeltaDecoders } from "./delta";
 import type { NewUserMsg } from "./frame/networkMessages/engine";
 
-export type DemoStateParser<A> = StatefulParser<DemoState, number, A>;
+export type DemoStateParser<A> = SP.StatefulParser<DemoState, number, A>;
+
+export const lift: <A>(p: P.Parser<number, A>) => DemoStateParser<A> = SP.lift;
 
 export type DemoState = {
   readonly deltaDecoders: ReadonlyMap<string, DeltaDecoder>;
