2c9d2b719fd69d6254deea7853ed85a4b515b99f
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-27 12:13:42 -0500

    refactor(parser): bounds check for seeking

--- packages/parser/src/Parser.ts
+++ packages/parser/src/Parser.ts
@@ -6,21 +6,22 @@ import {
 import { sequenceS, sequenceT } from "fp-ts/lib/Apply";
 import { pipe } from "fp-ts/lib/function";
 import * as P from "parser-ts/lib/Parser";
-import { success } from "./ParseResult";
+import { error, success } from "./ParseResult";
 import { stream } from "./Stream";
 
 export * from "parser-ts/lib/Parser";
 
-export const skip = <I>(offset: number): P.Parser<I, void> =>
-  pipe(
-    P.withStart(P.of<I, number>(offset)),
-    P.chain(([offset, i]) => seek(i.cursor + offset))
-  );
+export const skip =
+  <I>(offset: number): P.Parser<I, void> =>
+  (i) =>
+    pipe(i, seek(i.cursor + offset));
 
 export const seek =
   <I>(cursor: number): P.Parser<I, void> =>
   (i) =>
-    success(undefined, i, stream(i.buffer, cursor));
+    cursor < 0 || cursor > i.buffer.length
+      ? error(i, undefined, true)
+      : success(undefined, i, stream(i.buffer, cursor));
 
 export const manyN = <I, A>(
   parser: P.Parser<I, A>,
