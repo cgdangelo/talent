c9501501ad80f6951d95d7a9e203afdcd25a36df
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-01-28 00:06:11 -0500

    feat(parser-goldsrc): return all the bytes for a user message

--- packages/parser-goldsrc/src/frame/networkMessages/UserMessage.ts
+++ packages/parser-goldsrc/src/frame/networkMessages/UserMessage.ts
@@ -1,30 +1,47 @@
 import { parser as P, statefulParser as SP } from "@talent/parser";
 import { buffer as B } from "@talent/parser-buffer";
+import type { readonlyNonEmptyArray as RNEA } from "fp-ts";
 import { number, option as O, readonlyMap as RM } from "fp-ts";
 import { pipe } from "fp-ts/lib/function";
-import type { DemoState, DemoStateParser } from "../../DemoState";
+import * as DS from "../../DemoState";
 
-export type UserMessage = unknown;
+export type UserMessage = {
+  readonly id: number;
+  readonly name: string;
+  readonly data: RNEA.ReadonlyNonEmptyArray<number>;
+};
 
-const skipUserMessage: (messageId: number) => DemoStateParser<void> = (
-  messageId
-) =>
+const lookupUserMessage = RM.lookup(number.Eq);
+
+export const userMessage: (
+  messageId: number
+) => DS.DemoStateParser<UserMessage> = (messageId) =>
   pipe(
-    SP.get<number, DemoState>(),
+    SP.get<number, DS.DemoState>(),
     SP.chain(({ userMessages }) =>
       pipe(
-        userMessages,
-        RM.lookup(number.Eq)(messageId),
-        O.chain(O.fromPredicate(({ size }) => size > -1)),
-        O.fold(
-          () => SP.lift(B.uint8_le),
-          ({ size }) => SP.of(size)
-        )
+        lookupUserMessage(messageId)(userMessages),
+
+        // Found the user message from a previous SVC_NEWUSERMSG payload
+        O.map(({ index: id, name, size }) =>
+          pipe(
+            size > -1
+              ? // If we have an unsized message, read the length first
+                B.uint32_le
+              : // If we have a sized message, return the size
+                P.of(size),
+
+            DS.lift,
+
+            // We have a message size, so grab those bytes
+            SP.chain((size) => SP.take(size)),
+
+            SP.map((data) => ({ id, name, data }))
+          )
+        ),
+
+        // TODO Should we fail or just read the size and return as an "Unknown" message?
+        O.getOrElseW(() => SP.fail<DS.DemoState, number>())
       )
-    ),
-
-    SP.chain((customMessageSize) => SP.lift(P.skip(customMessageSize)))
+    )
   );
-
-export const userMessage: (messageId: number) => DemoStateParser<UserMessage> =
-  skipUserMessage;
