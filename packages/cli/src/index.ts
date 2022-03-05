a46d7160162865bd5e20de9dd3557ef2f36459c6
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-03-05 11:52:36 -0500

    refactor(cli): clean

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,42 +1,48 @@
 import { demo } from "@talent/parser-goldsrc/lib/Demo";
 import type { Stream } from "@talent/parser/lib/Stream";
 import { stream } from "@talent/parser/lib/Stream";
-import {
-  console,
-  either as E,
-  json,
-  predicate as P,
-  string as S,
-  taskEither as TE,
-} from "fp-ts";
+import * as Console from "fp-ts/lib/Console";
+import * as E from "fp-ts/lib/Either";
 import { constant, flow, pipe } from "fp-ts/lib/function";
+import * as json from "fp-ts/lib/Json";
+import { not } from "fp-ts/lib/Predicate";
+import * as S from "fp-ts/lib/string";
+import * as TE from "fp-ts/lib/TaskEither";
 import { readFile } from "fs/promises";
 
-const readFileContents: (
-  path: string
-) => TE.TaskEither<string, Stream<number>> = (path) =>
-  pipe(
-    TE.tryCatch(
-      () => readFile(path),
-      flow(E.toError, (a) => a.message)
-    ),
-    TE.map((buffer) => stream(buffer as unknown as Array<number>))
-  );
-
-const validateDemoPath: (path?: string) => E.Either<string, string> = flow(
+const validateDemoPath: (path?: string) => E.Either<Error, string> = flow(
   E.fromNullable("no demo path provided"),
-  E.map(S.trim),
-  E.chain(E.fromPredicate(P.not(S.isEmpty), constant("demo path is empty")))
+  E.chain(E.fromPredicate(not(S.isEmpty), constant("demo path is empty"))),
+  E.bimap(E.toError, S.trim)
 );
+
+const bufferToStream: (buffer: Buffer) => Stream<number> = (buffer) =>
+  stream(buffer as unknown as number[]);
+
+const readFileContents: (path: string) => TE.TaskEither<Error, Stream<number>> =
+  (path) =>
+    pipe(
+      TE.tryCatch(() => readFile(path), E.toError),
+      TE.map(bufferToStream)
+    );
+
+const parseDemo = flow(
+  demo,
+  E.bimap(
+    ({ expected }) => E.toError(expected),
+    ({ value }) => value
+  )
+);
+
+const serializeDemo = flow(json.stringify, E.mapLeft(E.toError));
 
 const main = pipe(
   validateDemoPath(process.argv[2]),
   TE.fromEither,
   TE.chain(readFileContents),
-  TE.chainEitherKW(demo),
-  TE.map(({ value }) => value),
-  TE.chainEitherK(json.stringify),
-  TE.chainIOK(console.log)
+  TE.chainEitherKW(parseDemo),
+  TE.chainEitherK(serializeDemo),
+  TE.chainIOK(Console.log)
 );
 
 main();
