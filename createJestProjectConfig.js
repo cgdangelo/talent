b324d0e0ef4894f4b262f7b068d81dec73605b20
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-12-27 04:52:11 -0500

    chore: move stuff

--- createJestProjectConfig.js
+++
@@ -1,6 +0,0 @@
-/** @type {(displayName: string) => import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports.createJestProjectConfig = (displayName = "") => ({
-  displayName,
-  preset: "ts-jest",
-  roots: ["test"],
-});
