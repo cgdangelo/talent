8e0467862aff3315887eeb9983e6d30ff8600943
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-12-30 01:57:17 -0500

    feat: emit directory entry

--- libraries/parser-goldsrc/src/Directory.ts
+++ libraries/parser-goldsrc/src/Directory.ts
@@ -1,11 +1,11 @@
-import { parser as P, statefulParser as SP } from "@talent/parser";
-import { buffer as B } from "@talent/parser-buffer";
-import { readonlyArray as RA } from "fp-ts";
-import { flow, pipe } from "fp-ts/lib/function";
-import * as DS from "./DemoState";
-import type { DirectoryEntry } from "./DirectoryEntry";
-import { directoryEntry } from "./DirectoryEntry";
-import { frames } from "./frame/Frame";
+import { parser as P, statefulParser as SP } from '@talent/parser';
+import { buffer as B } from '@talent/parser-buffer';
+import { readonlyArray as RA } from 'fp-ts';
+import { flow, pipe } from 'fp-ts/lib/function';
+import * as DS from './DemoState';
+import type { DirectoryEntry } from './DirectoryEntry';
+import { directoryEntry } from './DirectoryEntry';
+import { frames } from './frame/Frame';
 
 export type Directory = readonly DirectoryEntry[];
 
@@ -14,43 +14,37 @@ const totalEntries: B.BufferParser<number> = P.expected(
     B.int32_le,
     P.filter((a) => a >= 1 && a <= 1024)
   ),
-  "directory entries [1, 1024]"
+  'directory entries [1, 1024]'
 );
 
-const directoryEntries: (
-  totalEntries: number
-) => B.BufferParser<readonly DirectoryEntry[]> = (totalEntries) =>
-  P.manyN(directoryEntry, totalEntries);
+const directoryEntries: (totalEntries: number) => DS.DemoStateParser<readonly DirectoryEntry[]> = (
+  totalEntries
+) => SP.manyN(directoryEntry, totalEntries);
 
-const directoryEntriesWithoutFrames: (
-  directoryOffset: number
-) => B.BufferParser<Directory> = (directoryOffset) =>
+const directoryEntriesWithoutFrames: (directoryOffset: number) => DS.DemoStateParser<Directory> = (
+  directoryOffset
+) =>
   pipe(
-    P.seek<number>(directoryOffset),
-    P.apSecond(totalEntries),
-    P.chain(directoryEntries)
+    SP.seek<DS.DemoState, number>(directoryOffset),
+    SP.chain(() => SP.lift(totalEntries)),
+    SP.chain(directoryEntries)
   );
 
-const addFramesToDirectoryEntry: (
-  directoryEntry: DirectoryEntry
-) => DS.DemoStateParser<DirectoryEntry> = (directoryEntry) =>
+const addFramesToDirectoryEntry: (directoryEntry: DirectoryEntry) => DS.DemoStateParser<DirectoryEntry> = (
+  directoryEntry
+) =>
   pipe(
     DS.lift(P.seek<number>(directoryEntry.offset)),
     SP.chain(() => frames),
     SP.map((frames) => ({ ...directoryEntry, frames }))
   );
 
-const addFramesToDirectoryEntries: (
-  directoryEntries: Directory
-) => DS.DemoStateParser<Directory> = flow(
+const addFramesToDirectoryEntries: (directoryEntries: Directory) => DS.DemoStateParser<Directory> = flow(
   RA.map(addFramesToDirectoryEntry),
   RA.sequence(SP.Applicative)
 );
 
-export const directory: (
-  directoryOffset: number
-) => DS.DemoStateParser<Directory> = flow(
+export const directory: (directoryOffset: number) => DS.DemoStateParser<Directory> = flow(
   directoryEntriesWithoutFrames,
-  DS.lift,
   SP.chain(addFramesToDirectoryEntries)
 );
