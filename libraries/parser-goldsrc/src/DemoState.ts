716f43c2d430665d8f1b7859353ef58291faf9c3
Charles D'Angelo <c.g.dangelo@gmail.com>
2022-12-30 00:49:43 -0500

    feat: add event emitter to demo state

--- libraries/parser-goldsrc/src/DemoState.ts
+++ libraries/parser-goldsrc/src/DemoState.ts
@@ -1,9 +1,10 @@
-import type { parser as P } from "@talent/parser";
-import { statefulParser as SP } from "@talent/parser";
-import { readonlyMap as RM } from "fp-ts";
-import type { DeltaDecoder } from "./delta";
-import { initialDeltaDecoders } from "./delta";
-import type { NewUserMsg } from "./frame/networkMessages/engine";
+import type { parser as P } from '@talent/parser';
+import { statefulParser as SP } from '@talent/parser';
+import { readonlyMap as RM } from 'fp-ts';
+import type { DeltaDecoder } from './delta';
+import { initialDeltaDecoders } from './delta';
+import { IDemoEventEmitter } from './DemoEventEmitter';
+import type { NewUserMsg } from './frame/networkMessages/engine';
 
 export type DemoStateParser<A> = SP.StatefulParser<DemoState, number, A>;
 
@@ -11,14 +12,15 @@ export const lift: <A>(p: P.Parser<number, A>) => DemoStateParser<A> = SP.lift;
 
 export type DemoState = {
   readonly deltaDecoders: ReadonlyMap<string, DeltaDecoder>;
+  readonly eventEmitter?: IDemoEventEmitter;
   readonly maxClients: number;
   readonly networkProtocol: number;
-  readonly userMessages: ReadonlyMap<number, NewUserMsg["fields"]>;
+  readonly userMessages: ReadonlyMap<number, NewUserMsg['fields']>;
 };
 
 export const initialState: DemoState = {
   deltaDecoders: initialDeltaDecoders,
   maxClients: 0,
   networkProtocol: 0,
-  userMessages: RM.empty,
+  userMessages: RM.empty
 };
